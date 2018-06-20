02c5b3e07ab33b6ce0521c0d0c1b689b95bfb8de
Elena Zhuravleva <judy-iu@mail.ru>
2018-06-20 17:45:22 +0400

    Исправления.

--- js/main.js
+++ js/main.js
@@ -32,23 +32,20 @@ const arrowPrev = arrowButtons[0];
 const arrowNext = arrowButtons[1];
 
 
-const showScreen = (number) => {
+const showScreen = (element) => {
   main.innerHTML = ``;
-  main.appendChild(number);
+  main.appendChild(element);
 };
 
 let currentScreen = 0;
-const switchScreen = (number) => {
-  if (number < screensArr.length && number >= 0) {
-    number = number;
+const switchScreen = (element) => {
+ if (element < 0) {
+  element = screensArr.length - 1;
   }
-  else if (number < 0) {
-    number = screensArr.length - 1;
+  else if (element >= screensArr.length) {
+    element = 0;
   }
-  else if (number >= screensArr.length) {
-    number = 0;
-  }
-  currentScreen = number;
+  currentScreen = element;
   showScreen(screensArr[currentScreen]);
 };
 
